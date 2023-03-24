cdfbe6100d9650d8bf139bc30e2e7aebc9932efa
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2023-03-24 17:28:24 -0500

    got basic search working

--- src/pages/api/movies/search.ts
+++ src/pages/api/movies/search.ts
@@ -1,6 +1,9 @@
 // Next
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+// axios
+import axios from 'axios';
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     res.status(401).json({ message: 'Invalid request.' });
@@ -9,7 +12,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   if (req.method === 'GET') {
     if (req.query && req.query.q && typeof req.query.q === 'string') {
-      res.status(200).json(req.query.q);
+      const searchStr = req.query.q;
+      try {
+        const { data } = await axios.get(`${process.env.OMDB_URL}?t=${searchStr}}&apikey=${process.env.OMDB_APIKEY}`);
+        console.log(data);
+        res.status(200).json(req.query.q);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Unabled to search movies' });
+      }
       return;
     }
 
